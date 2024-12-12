d398dc14d86922f51ae8efd3aa3c2598b1235cc3
Carlos Pujades <cpcarlospujades@gmail.com>
2024-12-12 09:15:36 +0100

    Done bounce exercise

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100
+bounce_height = 0.6
+bounce_count = 0
+
+for bounce in range(10):
+    bounce_count += 1
+    height *= bounce_height
+    print(bounce_count, round(height, 4))
