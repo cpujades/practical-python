70e7f62030336f64c4a12ecbd7e9c5bca04a2535
Carlos Pujades <cpcarlospujades@gmail.com>
2024-12-16 12:14:15 +0100

    Finished exercise 1_33

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,14 +2,30 @@
 #
 # Exercise 1.27
 
-total_price = 0.0
+import csv
+import sys
 
-with open("Data/portfolio.csv", "rt") as f:
-    headers = next(f)
-    for line in f:
-        row = line.split(",")
-        shares = int(row[1])
-        price = float(row[2])
-        total_price += shares * price
 
-print(f"Total cost: {total_price}")
+def portfolio_cost(filename):
+    tot_price = 0.0
+    try:
+        with open(filename, "rt") as f:
+            rows = csv.reader(f)
+            headers = next(rows)
+            for line in f:
+                row = line.split(",")
+                shares = int(row[1])
+                price = float(row[2])
+                tot_price += shares * price
+    except ValueError as e:
+        print(e)
+    return tot_price
+
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = "Data/portfolio.csv"
+
+cost = portfolio_cost(filename)
+print(f"Total cost: {cost}")
