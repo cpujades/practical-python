a6651ca653189e90a34e3f3e752491583276a9ae
Carlos Pujades <cpcarlospujades@gmail.com>
2024-12-16 22:54:39 +0100

    Completed exercise 2_16 (and 2_15)

---
+++ Work/2_15/pcost.py
@@ -0,0 +1,32 @@
+# pcost.py
+#
+# Exercise 1.33
+
+import csv
+import sys
+
+
+def portfolio_cost(filename):
+    tot_price = 0.0
+
+    with open(filename, "rt") as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for idx, row in enumerate(rows):
+            try:
+                shares = int(row[1])
+                price = float(row[2])
+                tot_price += shares * price
+            except ValueError:
+                print(f"Row {idx + 1}: Couldn't convert: {row}")
+
+    return tot_price
+
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = "Data/portfolio.csv"
+
+cost = portfolio_cost(filename)
+print(f"Total cost: {cost}")
