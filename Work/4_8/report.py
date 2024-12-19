135c3eb430c286344dc8532434e64e44fd28f548
Carlos Pujades <cpcarlospujades@gmail.com>
2024-12-19 12:33:41 +0100

    Completed exercise 4_8

---
+++ Work/4_8/report.py
@@ -0,0 +1,80 @@
+# fileparse.py
+#
+# Exercise 3.18
+
+import csv
+import sys
+import os
+from stock import Stock
+from tableformat import TableFormatter
+
+# Now you can import the parse_csv function from fileparse
+from fileparse import parse_csv
+
+
+def read_portfolio(filename):
+    """
+    Read a stock portfolio file into a list of dictionaries with keys
+    name, shares, and price.
+    """
+    with open(filename) as file:
+        portdicts = parse_csv(
+            file, select=["name", "shares", "price"], types=[str, int, float]
+        )
+    portfolio = [Stock(d["name"], d["shares"], d["price"]) for d in portdicts]
+
+    return portfolio
+
+
+def read_prices(filename):
+    """
+    Read a CSV file of prices into a dict mapping names to prices.
+    """
+    with open(filename) as file:
+        stocks = parse_csv(file, types=[str, float], has_headers=False)
+
+    return stocks
+
+
+def make_report(portfolio, prices):
+    report = []
+    for s in portfolio:
+        name = s.name
+        shares = s.shares
+        price = s.price
+        current_price = [price for stock, price in prices if stock == name][0]
+        change = current_price - price
+        report.append((name, shares, current_price, change))
+    return report
+
+
+def print_report(report_data, formatter):
+    """
+    Print a nicely formatted table from a list of (name, shares, price, change)
+    tuples.
+    """
+    formatter.headings(["Name", "Shares", "Price", "Change"])
+    for name, shares, price, change in report_data:
+        price = "$" + str(round(price, 2))
+        change = round(change, 2)
+        row_data = [name, str(shares), price, change]
+        formatter.row(row_data)
+    print()
+
+
+def portfolio_report(portfolio_filename, prices_filename, fmt="txt"):
+    portfolio = read_portfolio(portfolio_filename)
+    prices = read_prices(prices_filename)
+    report = make_report(portfolio, prices)
+    formatter = TableFormatter.create_formatter(fmt)
+    print_report(report, formatter)
+
+
+def main(argv):
+    if len(argv) != 4:
+        raise SystemExit(f"Usage: {argv[0]} portfoliofile pricesfile")
+    portfolio_report(argv[1], argv[2], argv[3])
+
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ Work/4_9/pcost.py
@@ -0,0 +1,33 @@
+# pcost.py
+#
+# Exercise 3.18
+
+import csv
+import sys
+import os
+
+from report import read_portfolio
+
+
+def portfolio_cost(filename):
+    tot_price = 0.0
+
+    portfolio = read_portfolio(filename)
+    for stock in portfolio:
+        tot_price += stock.shares * stock.price
+
+    return tot_price
+
+
+def main(sys_argv):
+    if len(sys_argv) == 2:
+        filename = sys_argv[1]
+    else:
+        filename = "Data/portfolio.csv"
+
+    cost = portfolio_cost(filename)
+    print(f"Total cost: {cost}")
+
+
+if __name__ == "__main__":
+    main(sys.argv)
