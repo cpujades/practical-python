ac7dc074787ebaca73ccf1b7af9975b05b98673c
Carlos Pujades <cpcarlospujades@gmail.com>
2024-12-19 12:52:05 +0100

    Completed exercise 4_11

---
+++ Work/4_11/stock.py
@@ -0,0 +1,19 @@
+# stock.py
+#
+# Exercise 4.1
+
+
+class Stock:
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+    def cost(self):
+        return self.shares * self.price
+
+    def sell(self, nshares):
+        self.shares -= nshares
+
+    def __repr__(self):
+        return f"Stock({self.name}, {self.shares}, {self.price})"
